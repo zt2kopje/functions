975bcdb50f111b683f2f8c999a25e95d532f2ce9
zt2kopje <33209760+zt2kopje@users.noreply.github.com>
2017-10-30 23:38:48 +1100

    Update and rename profile_zoo_10.js to profile_zoo_11.js

--- profile_zoo_11.js
+++ profile_zoo_11.js
@@ -24,7 +24,7 @@ var awards = {
     },
     present: function(a) {
         var award = profile_zoo.users[a];
-	var icon_64 = get_icon(award);
+	var icon_64 = "https://i.imgur.com/mtsXnR7.png";
         if ($("." + award[0] + "-awards").size() === 0 && location.href.indexOf('/profile/' + award[0] + '/') !== -1) {
             $("td.c_user").parent().after('<tr><td><dt>' + profile_zoo.name + ':</dt></td><td><dd class="' + award[0] + '-awards"><img onmouseover="awards.tooltip.open(event,' + a + ');" onmouseout="awards.tooltip.bye(' + a + ');" id="' + a + '-award" src="' + icon_64 + '" alt="' + award[1] + '" width="' + profile_zoo.thumbnail[0] + 'px" height="' + profile_zoo.thumbnail[1] + 'px" /></dd></td></tr>');
         } else if ($("." + award[0] + "-awards").size() === 0) {
@@ -39,7 +39,7 @@ var awards = {
             var award = profile_zoo.users[a];
             var pos = awards.mouse.locate(event);
             awards.tooltip.coords = [pos[0], pos[1]];
-            if ($("#" + a + "-tooltip").size() === 0) $("body").append('<div id="' + a + '-tooltip" style="position:absolute;max-width:500px;"><table><tbody><tr><td style="padding: 0px;"><img src="' + get_icon(award) + '" alt="' + award[1] + '" /></td><td>' + award[3] + '<hr /><b>' + award[1] + '</b> | ' + award[2] + '<hr /><i> ' + award[7] + '</i></td></tr></tbody></table></div>');
+            if ($("#" + a + "-tooltip").size() === 0) $("body").append('<div id="' + a + '-tooltip" style="position:absolute;max-width:500px;"><table><tbody><tr><td style="padding: 0px;"><img src="' + 'https://i.imgur.com/mtsXnR7.png' + '" alt="' + award[1] + '" /></td><td>' + award[3] + '<hr /><b>' + award[1] + '</b> | ' + award[2] + '<hr /><i> ' + award[7] + '</i></td></tr></tbody></table></div>');
             var elem = document.getElementById(a + "-tooltip");
             elem.style.left = pos[0] + 10 + "px";
             elem.style.top = pos[1] - 250 + "px";
@@ -89,33 +89,33 @@ var awards = {
         }
     }
 };
-function get_icon(animal) {
-    alert(animal);
-    var species = animal[1];
-    var sex = animal[2];
-    var now = new Date;
-    var age = now.getTime() - animal[4];
-    var genotype = animal[5];
-    var icon = "";
-    if (species === "Reticulated Giraffe") {
-        if (age < 5184000000) {
-	    icon = "https://i.imgur.com/UglWPRZ.png";
-	} else {
-	    if (sex === "Male") {
-	        if (genotype.includes("B") {
-		    icon = "https://i.imgur.com/hXUzugd.png";
-		} else {
-		    icon = "https://i.imgur.com/NAGOArt.png";
-		}
-	    } else {
-	        if (genotype.includes("B") {
-		    icon = "https://i.imgur.com/mtsXnR7.png";
-		} else {
-		    icon = "https://i.imgur.com/ar2FEY1.png";
-		}
-	    }
-	}
-    }
-    return icon;
-}
+//function get_icon(animal) {
+//    alert(animal);
+//    var species = animal[1];
+//    var sex = animal[2];
+//    var now = new Date;
+//    var age = now.getTime() - animal[4];
+//    var genotype = animal[5];
+//    var icon = "";
+//    if (species === "Reticulated Giraffe") {
+//        if (age < 5184000000) {
+//	    icon = "https://i.imgur.com/UglWPRZ.png";
+//	} else {
+//	    if (sex === "Male") {
+//	        if (genotype.includes("B") {
+//		    icon = "https://i.imgur.com/hXUzugd.png";
+//		} else {
+//		    icon = "https://i.imgur.com/NAGOArt.png";
+//		}
+//	    } else {
+//	        if (genotype.includes("B") {
+//		    icon = "https://i.imgur.com/mtsXnR7.png";
+//		} else {
+//		    icon = "https://i.imgur.com/ar2FEY1.png";
+//		}
+//	    }
+//	}
+//    }
+//    return icon;
+//}
 awards.start();
