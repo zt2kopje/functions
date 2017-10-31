ac6d80c52ba66566640d86ea8b9e2851187b81d8
zt2kopje <33209760+zt2kopje@users.noreply.github.com>
2017-10-31 14:29:47 +1100

    Update and rename profile_data.js to profile_data_2.js

--- profile_data_2.js
+++ profile_data_2.js
@@ -1,11 +1,13 @@
 var profile_zoo = {
   name : "Animal Collection",
-  thumbnail : [16, 16],
   closeFunction : "fade",
   users : [
     ["5063823", "Reticulated Giraffe", "Male", "Simon", 1509368099413, "BB", 0.8, "1st Place - Royal Giraffe Parade"],
     ["5063823", "Reticulated Giraffe", "Female", "Cinderella", 150936809941, "Bb", 0.8, "2nd Place - Royal Giraffe Parade"],
     ["5063823", "Reticulated Giraffe", "Female", "Georgia", 150936809941, "bb", 0.8, "Escaped from a Circus"],
     ["5063823", "Reticulated Giraffe", "Male", "George", 150936809941, "bb", 0.8, "Eloped with Georgia"],
+    ["5063823", "Bengal Tiger", "Female", "Binjai", 150936809941, "ww", 0.9, "No reason"],
+    ["5063823", "Bengal Tiger", "Male", "Ramalon", 150936809941, "Ww", 3, "No reason"],
+    ["5063821", "Mountain Gorilla", "Male", "Harambe", 150936809941, "BB", 0.8, "No reason"],
   ]
 };
