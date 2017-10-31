7f36060649cd76201ab33b6d5fb2ccaac86534e3
zt2kopje <33209760+zt2kopje@users.noreply.github.com>
2017-10-31 13:06:44 +1100

    Create profile_data.js

---
+++ profile_data.js
@@ -0,0 +1,11 @@
+var profile_zoo = {
+  name : "Animal Collection",
+  thumbnail : [16, 16],
+  closeFunction : "fade",
+  users : [
+    ["5063823", "Reticulated Giraffe", "Male", "Simon", 1509368099413, "BB", 0.8, "1st Place - Royal Giraffe Parade"],
+    ["5063823", "Reticulated Giraffe", "Female", "Cinderella", 150936809941, "Bb", 0.8, "2nd Place - Royal Giraffe Parade"],
+    ["5063823", "Reticulated Giraffe", "Female", "Georgia", 150936809941, "bb", 0.8, "Escaped from a Circus"],
+    ["5063823", "Reticulated Giraffe", "Male", "George", 150936809941, "bb", 0.8, "Eloped with Georgia"],
+  ]
+};
