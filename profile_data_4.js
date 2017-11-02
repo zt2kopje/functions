d035dcbe93ec94ac6387212813f32ea1adf98ed4
zt2kopje <33209760+zt2kopje@users.noreply.github.com>
2017-11-02 23:12:53 +1100

    Update and rename profile_data_3.js to profile_data_4.js

--- profile_data_4.js
+++ profile_data_4.js
@@ -10,5 +10,6 @@ var profile_zoo = {
     ["5063823", "Bengal Tiger", "Male", "Ramalon", 150936809941, "Ww", 3, "No reason"],
     ["5063821", "Mountain Gorilla", "Male", "Harambe", 150936809941, "BB", 0.8, "No reason"],
     ["5063821", "Ring-Tailed Lemur", "Male", "Toes", 1506457900061, "BB", 1.76, "Purchased for $400."],
+    ["5063821", "Nile Crocodile", "Male", "Snappy", 1507319169332, "BB", 1.69, "Purchased for $900."],
   ]
 };
