c3656e0a14c8db4a9653901f724f6e93e05a5ced
zt2kopje <33209760+zt2kopje@users.noreply.github.com>
2017-11-02 22:59:33 +1100

    Update and rename get_icons_39.js to get_icons_40.js

--- get_icons_40.js
+++ get_icons_40.js
@@ -130,6 +130,36 @@ function get_icon(animal) {
 	        icon = "africanlion-female-white";
 	    }
 	}
+    } else if (species === "Alpine Ibex") {
+        if (age < 2419200000) {
+	    icon = "alpineibex-young";
+	} else {
+	    switch(now.getMonth()) {
+		case 10:
+		case 11:
+		case 12:
+                case 1:
+                case 2:
+		case 3:
+                    if(sex === "Male") {
+		        icon = "alpineibex-male-winter";
+		    } else {
+		        icon = "alpineibex-female-winter";
+		    }
+                    break;
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                case 9:
+                    if(sex === "Male") {
+		        icon = "alpineibex-male";
+		    } else {
+		        icon = "alpineibex-female";
+		    }
+            }
+	}
     } else if (species === "American Beaver") {
         if (age < 1209600000) {
 	    icon = "americanbeaver-young";
