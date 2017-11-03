38e1509f2e43d06477d30ee95e62a34b5b06dc58
zt2kopje <33209760+zt2kopje@users.noreply.github.com>
2017-11-03 23:59:52 +1100

    Update and rename profile_zoo_50.js to profile_zoo_51.js

--- profile_zoo_51.js
+++ profile_zoo_51.js
@@ -29,8 +29,12 @@ var awards = {
         open: function(event, a) {
             var award = profile_zoo.users[a];
             var pos = awards.mouse.locate(event);
+	    var icon_64 = get_icon(award);
+	    if (icon_64.includes("-egg")) {
+	        award[2] = "Egg";
+	    }
             awards.tooltip.coords = [pos[0], pos[1]];
-            if ($("#" + a + "-tooltip").size() === 0) $("body").append('<div id="' + a + '-tooltip" style="position:absolute;max-width:500px;"><table><tbody><tr><td style="padding: 0px;"><div class="' + get_icon(award) + '" alt="' + award[1] + '"></div></td><td>' + award[3] + '<hr /><b>' + award[1] + '</b> | ' + award[2] + '<hr /><i> ' + award[7] + '</i></td></tr></tbody></table></div>');
+            if ($("#" + a + "-tooltip").size() === 0) $("body").append('<div id="' + a + '-tooltip" style="position:absolute;max-width:500px;"><table><tbody><tr><td style="padding: 0px;"><div class="' + icon_64 + '" alt="' + award[1] + '"></div></td><td>' + award[3] + '<hr /><b>' + award[1] + '</b> | ' + award[2] + '<hr /><i> ' + award[7] + '</i></td></tr></tbody></table></div>');
             var elem = document.getElementById(a + "-tooltip");
             elem.style.left = pos[0] + 25 + "px";
             elem.style.top = pos[1] - 25 + "px";
