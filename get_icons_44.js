6c369cfafee6f33cf188fb300778d1bb774d754b
zt2kopje <33209760+zt2kopje@users.noreply.github.com>
2017-11-04 18:55:35 +1100

    Update and rename get_icons_43.js to get_icons_44.js

--- get_icons_44.js
+++ get_icons_44.js
@@ -42,7 +42,7 @@ var animal_data = {
 	["Bengal Tiger", 4, 3.5, 1700, "bengaltiger", "Y"],
 	["Black Rhinoceros", 5, 1.55, 2100, "blackrhinoceros", "Y"],
 	["Chimpanzee", 3, 1.75, 1400, "chimpanzee", "Y"],
-	["Dromedary Camel", 1, 1.65, 600, "dromedarycamel", "Y"],
+	["Dromedary Camel", 1, 1.65, 600, "dromedary", "Y"],
 	["Emperor Penguin", 2, 1, 1000, "emperorpenguin", "Y"],
 	["Gemsbok", 3, 1.6, 1300, "gemsbok", "Y"],
 	["Giant Panda", 5, 1.75, 2200, "giantpanda", "Y"],
